7c733aacb35423c50e0fb3f6e0d114282b033bb4
Muhammad Fariz <mfarix730@gmail.com>
2025-10-15 22:26:16 +0700

    starter

---
+++ src/components/ui/combobox.tsx
@@ -0,0 +1,91 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import {
+    Command,
+    CommandEmpty,
+    CommandGroup,
+    CommandInput,
+    CommandItem,
+    CommandList,
+} from "@/components/ui/command"
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover"
+import { cn } from "@/lib/utils"
+import { CheckIcon, ChevronsUpDownIcon } from "lucide-react"
+import * as React from "react"
+
+type ComboBoxItem = { value: string; label: string }
+
+type ComboBoxProps = {
+  items?: ComboBoxItem[]
+  placeholder?: string
+  onChange?: (value: string) => void
+  defaultValue?: string
+  width?: string
+}
+
+export function ComboBox({
+  items = [],
+  placeholder = "Select an option...",
+  onChange,
+  defaultValue = "",
+  width = "w-[200px]",
+}: ComboBoxProps) {
+  const [open, setOpen] = React.useState(false)
+  const [value, setValue] = React.useState<string>(defaultValue)
+
+  const selectedItem = items.find((item: ComboBoxItem) => item.value === value)
+
+  const handleSelect = (currentValue: string) => {
+    const newValue = currentValue === value ? "" : currentValue
+    setValue(newValue)
+    setOpen(false)
+    onChange?.(newValue)
+  }
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className={`${width} justify-between`}
+        >
+          {selectedItem ? selectedItem.label : placeholder}
+          <ChevronsUpDownIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+
+      <PopoverContent className={`${width} p-0`}>
+        <Command>
+          <CommandInput placeholder="Search..." />
+          <CommandList>
+            <CommandEmpty>No item found.</CommandEmpty>
+            <CommandGroup>
+              {items.map((item) => (
+                <CommandItem
+                  key={item.value}
+                  value={item.value}
+                  onSelect={handleSelect}
+                >
+                  <CheckIcon
+                    className={cn(
+                      "mr-2 h-4 w-4",
+                      value === item.value ? "opacity-100" : "opacity-0"
+                    )}
+                  />
+                  {item.label}
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  )
+}
